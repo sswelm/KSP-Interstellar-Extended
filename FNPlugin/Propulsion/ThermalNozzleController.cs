215020a2ea67ead171bd31d2fd980ae66b0f9af6
dunclaw <dunclaw@hotmail.com>
2016-12-04 11:50:12 -0800

    Local Fixes

--- FNPlugin/Propulsion/ThermalNozzleController.cs
+++ FNPlugin/Propulsion/ThermalNozzleController.cs
@@ -1399,9 +1399,24 @@ namespace FNPlugin
                 partresources.ForEach(partresource => currentintakeatm += partresource.amount);
                 partresources.ForEach(partresource => maxintakeatm += partresource.maxAmount);
 
-                intake_amounts[resourcename] = currentintakeatm;
-                intake_maxamounts[resourcename] = maxintakeatm;
-            }
+				// to smooth out occilations in resource production, take a 90/10 weighted average of the previous frame production
+				if (intake_amounts.ContainsKey(resourcename))
+				{
+					intake_amounts[resourcename] = (intake_amounts[resourcename] * 0.99) + (currentintakeatm * 0.01);
+				}
+				else
+				{
+					intake_amounts[resourcename] = currentintakeatm;
+				}
+				if (intake_maxamounts.ContainsKey(resourcename))
+				{
+					intake_maxamounts[resourcename] = (intake_maxamounts[resourcename] * 0.99) + (maxintakeatm * 0.01);
+				}
+				else
+				{
+					intake_maxamounts[resourcename] = maxintakeatm;
+				}
+			}
 
             if (intake_amounts.ContainsKey(resourcename))
                 return Math.Max(intake_amounts[resourcename], 0);
@@ -1448,10 +1463,15 @@ namespace FNPlugin
                     nozzle.Static_updating2 = false;
                 }
 
-                if (fuel_flow_amounts.ContainsKey(resourcename))
-                    fuel_flow_amounts[resourcename] = enum_rate;
-                else
-                    fuel_flow_amounts.Add(resourcename, enum_rate);
+				// to avoid occilation, take weighted average with previous frame
+				if (fuel_flow_amounts.ContainsKey(resourcename))
+				{
+					fuel_flow_amounts[resourcename] = (fuel_flow_amounts[resourcename] * 0.9) + (enum_rate * 0.1);
+				}
+				else
+				{
+					fuel_flow_amounts.Add(resourcename, enum_rate);
+				}
             }
 
             if (fuel_flow_amounts.ContainsKey(resourcename))
