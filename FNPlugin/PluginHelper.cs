1cb1daa050677a57491684606e1d663a650b5a5f
sswelm <sswelm@gmail.com>
2018-09-30 22:44:29 +0200

    reactiveated docking triggers

--- FNPlugin/PluginHelper.cs
+++ FNPlugin/PluginHelper.cs
@@ -26,8 +26,8 @@ namespace FNPlugin
 
             GameEvents.onGameStateSaved.Add(OnGameStateSaved);
             GameEvents.onVesselSituationChange.Add(OnVesselSituationChange);
-            //GameEvents.onDockingComplete.Add(OnDockingComplete);
-            //GameEvents.onPartDeCoupleComplete.Add(OnPartDeCoupleComplete);
+            GameEvents.onDockingComplete.Add(OnDockingComplete);
+            GameEvents.onPartDeCoupleComplete.Add(OnPartDeCoupleComplete);
 
             Debug.Log("[KSPI] - GameEventSubscriber Initialised");
         }
