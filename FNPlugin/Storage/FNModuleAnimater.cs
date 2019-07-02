fc568cb319e2dc7cac37310269d46ebf07d0901a
FreeThinker <sswelm@gmail.com>
2019-07-03 01:51:51 +0200

    Added delpyable heatshield

---
+++ FNPlugin/Storage/FNModuleAnimater.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using UnityEngine;
+
+/* AdvancedAnimator was made by Christophe Savard (stupid_chris) and is licensed under CC-BY-SA. You are free to share and modify this code freely
+ * under the attribution clause to me. You can contact me on the forums for more information. */
+
+namespace FNPlugin
+{
+    public class FNModuleAnimator : PartModule
+    {
+        #region KSPFields
+        [KSPField]
+        public string animationName = string.Empty;
+        [KSPField]
+        public string guiEnableName = string.Empty;
+        [KSPField]
+        public string guiDisableName = string.Empty;
+        [KSPField]
+        public string actionEnableName = string.Empty;
+        [KSPField]
+        public string actionDisableName = string.Empty;
+        [KSPField]
+        public string actionToggleName = string.Empty;
+        [KSPField]
+        public float animationSpeed = 1f;
+        [KSPField]
+        public bool oneShot = false;
+        [KSPField]
+        public bool activeEditor = true;
+        [KSPField]
+        public bool activeFlight = true;
+        [KSPField]
+        public bool activeUnfocused = true;
+        [KSPField]
+        public float unfocusedRange = 5f;
+        [KSPField(isPersistant = true)]
+        public bool enabled = false;
+        [KSPField(isPersistant = true)]
+        public bool played = true;
+        [KSPField(guiActive = true, guiName = "Status")]
+        public string status = "Enable";
+        #endregion
+
+        #region Fields
+        private bool initiated = false;
+        #endregion
+
+        #region Part GUI
+        [KSPEvent(active = true, guiActive = true, guiActiveEditor = true, guiActiveUnfocused = true, externalToEVAOnly = true, guiName = "Toggle", unfocusedRange = 5)]
+        public void GUIToggle()
+        {
+            if (this.enabled) { Disable(); }
+            else { Enable(); }
+        }
+        #endregion
+
+        #region Action Groups
+        [KSPAction("Enable")]
+        public void ActionEnable(KSPActionParam param)
+        {
+            Enable();
+        }
+
+        [KSPAction("Disable")]
+        public void ActionDisable(KSPActionParam param)
+        {
+            Disable();
+        }
+
+        [KSPAction("Toggle")]
+        public void ActionToggle(KSPActionParam param)
+        {
+            GUIToggle();
+        }
+        #endregion
+
+        #region Methods
+        private void Enable()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                if (this.oneShot && this.played) { return; }
+                this.played = true;
+            }
+            if (CheckAnimationPlaying()) { PlayAnimation(this.animationSpeed, GetAnimationTime()); }
+            else { PlayAnimation(this.animationSpeed, 0); }
+            this.enabled = true;
+            this.part.Effect("onAnimationDeploy");
+            SetName();
+        }
+
+        private void Disable()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                if (this.oneShot && this.played) { return; }
+                this.played = true;
+            }
+            if (CheckAnimationPlaying()) { PlayAnimation(-this.animationSpeed, GetAnimationTime()); }
+            else { PlayAnimation(-this.animationSpeed, 1); }
+            Events["GUIToggle"].guiName = guiDisableName;
+            this.enabled = false;
+            this.part.Effect("onAnimationRetract");
+            SetName();
+        }
+
+        private void SetName()
+        {
+            BaseEvent toggle = Events["GUIToggle"];
+            if (!string.IsNullOrEmpty(this.guiEnableName) && !string.IsNullOrEmpty(this.guiDisableName)) { toggle.guiName = this.enabled ? this.guiDisableName : this.guiEnableName; }
+        }
+
+        private void InitiateAnimation()
+        {
+            foreach (Animation animation in this.part.FindModelAnimators(this.animationName))
+            {
+                AnimationState state = animation[this.animationName];
+                state.normalizedTime = this.enabled ? 1 : 0;
+                state.normalizedSpeed = 0;
+                state.enabled = false;
+                state.wrapMode = WrapMode.Clamp;
+                state.layer = 1;
+                animation.Play(this.animationName);
+            }
+            this.initiated = true;
+        }
+
+        private void PlayAnimation(float animationSpeed, float animationTime)
+        {
+            //Plays the animation
+            foreach (Animation animation in this.part.FindModelAnimators(this.animationName))
+            {
+                AnimationState state = animation[this.animationName];
+                state.normalizedTime = animationTime;
+                state.normalizedSpeed = animationSpeed;
+                state.enabled = true;
+                state.wrapMode = WrapMode.Clamp;
+                animation.Play(this.animationName);
+            }
+        }
+
+        private bool CheckAnimationPlaying()
+        {
+            //Checks if a given animation is playing
+            foreach (Animation animation in this.part.FindModelAnimators(this.animationName))
+            {
+                return animation.IsPlaying(this.animationName);
+            }
+            return false;
+        }
+
+        private float GetAnimationTime()
+        {
+            foreach (Animation animation in this.part.FindModelAnimators(this.animationName))
+            {
+                return animation[this.animationName].normalizedTime;
+            }
+            return 0f;
+        }
+        #endregion
+
+        #region Functions
+        private void Update()
+        {
+            if (!HighLogic.LoadedSceneIsFlight) { return; }
+            if (CheckAnimationPlaying())
+            {
+                if (this.enabled) { this.status = "Deploying..."; }
+                else { this.status = "Retracting..."; }
+            }
+            else
+            {
+                if (this.oneShot && this.played) { this.status = "Locked."; }
+                else
+                {
+                    if (this.enabled) { this.status = "Deployed"; }
+                    else { this.status = "Retracted"; }
+                }
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (!HighLogic.LoadedSceneIsFlight) { return; }
+            if (this.initiated)
+            {
+                foreach (Animation animation in this.part.FindModelAnimators(animationName))
+                {
+                    animation.Stop(animationName);
+                }
+                this.initiated = false;
+            }
+        }
+        #endregion
+
+        #region Overrides
+        public override void OnStart(PartModule.StartState state)
+        {
+            if (!HighLogic.LoadedSceneIsFlight && !HighLogic.LoadedSceneIsEditor) { return; }
+
+            //In case of errors
+            if (string.IsNullOrEmpty(this.animationName) || this.part.FindModelAnimators(this.animationName).Length <= 0)
+            {
+                Events.ForEach(e => e.active = false);
+                Actions.ForEach(a => a.active = false);
+                return;
+            }
+
+            //Initiates the animation
+            InitiateAnimation();
+
+            //Sets the action groups/part GUI
+            BaseEvent toggle = Events["GUIToggle"];
+            BaseAction aEnable = Actions["ActionEnable"], aDisable = Actions["ActionDisable"], aToggle = Actions["ActionToggle"];
+
+            toggle.guiActiveEditor = this.activeEditor;
+            toggle.guiActive = this.activeFlight;
+            toggle.guiActiveUnfocused = this.activeUnfocused;
+            toggle.unfocusedRange = this.unfocusedRange;
+            SetName();
+
+            if (string.IsNullOrEmpty(this.actionEnableName)) { aEnable.active = false; }
+            else { aEnable.guiName = this.actionEnableName; }
+
+            if (string.IsNullOrEmpty(this.actionDisableName)) { aDisable.active = false; }
+            else { aDisable.guiName = this.actionDisableName; }
+
+            if (string.IsNullOrEmpty(this.actionToggleName)) { aToggle.active = false; }
+            else { aToggle.guiName = this.actionToggleName; }
+        }
+        #endregion
+    }
+}
---
+++ GameData/WarpPlugin/Parts/Stuctural/DeployableShield/OLDD_README.txt
@@ -0,0 +1,30 @@
+OLDD Mod Parts Pack
+
+Version:	1.0	(KSP 0.25.x)
+
+Author: 	DennyTX  tfa_denny@mail.ru
+
+License:	Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+		http://www.creativecommons.org/licenses/by-sa/4.0/
+
+Disclaimer:	Use at own risk ;). 
+
+
+===Dependencies===
+ - Deadly Reentry Continued plugin (NathanKell) - if installed, the Heat Shield will protect your entry devices from the heat and stresses of atmospheric reentry. 
+   And will increase drag when open.
+
+===How To Install===
+
+1. Ensure that you have all of the aforementioned dependencies installed
+
+2. Remove any previous version
+
+3. Extract the .ZIP archive and copy the GameData folder provided into your KSP root directory
+
+The parts should then be located under the OLDD/NASA folder
+
+
+===Release Notes===
+1.0        26 October 2014
+
