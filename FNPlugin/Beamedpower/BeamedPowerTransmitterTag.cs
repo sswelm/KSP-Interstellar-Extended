1b4c9b81802520c2107859ceca3486db0378a922
FreeThinker <sswelm@gmail.com>
2019-07-11 21:29:25 +0200

    Added TransmitterTag to Preserve Savegames

---
+++ FNPlugin/Beamedpower/BeamedPowerTransmitterTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace FNPlugin.Beamedpower
+{
+    public class MirrorRelaysTransmitterTag : BeamedPowerTransmitterTag { };
+
+    public class PhasedArrayTransmitterTag : BeamedPowerTransmitterTag {};
+
+    public class MicrowavePowerTransmitterTag : BeamedPowerTransmitterTag {};
+
+    public class BeamedPowerLaserTransmitterTag : BeamedPowerTransmitterTag { };
+
+    public class BeamedPowerTransmitterTag : PartModule {}
+}
