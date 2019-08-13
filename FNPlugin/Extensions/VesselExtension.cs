f0334d142a43674a9d690be369fd7664b0277799
sswelm <sswelm@gmail.com>
2019-08-13 14:36:22 +0200

    cleaned code

--- FNPlugin/Extensions/VesselExtension.cs
+++ FNPlugin/Extensions/VesselExtension.cs
@@ -16,32 +16,42 @@ namespace FNPlugin.Extensions
             var universalTime = Planetarium.GetUniversalTime();
             var vesselPosition = vessel.orbit.getPositionAtUT(universalTime);
 
-            if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.Prograde)
-                requestedDirection = vessel.obt_velocity.normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.Retrograde)
-                requestedDirection = -vessel.obt_velocity.normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.Maneuver)
-                requestedDirection = vessel.patchedConicSolver.maneuverNodes.Count > 0 ? vessel.patchedConicSolver.maneuverNodes[0].GetBurnVector(vessel.orbit) : vessel.obt_velocity.normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.Target)
-                requestedDirection = (vessel.targetObject.GetOrbit().getPositionAtUT(universalTime) - vesselPosition).normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.AntiTarget)
-                requestedDirection = -(vessel.targetObject.GetOrbit().getPositionAtUT(universalTime) - vesselPosition).normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.Normal)
-                requestedDirection = Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position)).normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.Antinormal)
-                requestedDirection = -Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position)).normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.RadialIn)
-                requestedDirection = -Vector3.Cross(vessel.obt_velocity, Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position))).normalized;
-            else if (vessel.Autopilot.Mode == VesselAutopilot.AutopilotMode.RadialOut)
-                requestedDirection = Vector3.Cross(vessel.obt_velocity, Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position))).normalized;
+            switch (vessel.Autopilot.Mode)
+            {
+                case VesselAutopilot.AutopilotMode.Prograde:
+                    requestedDirection = vessel.obt_velocity.normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.Retrograde:
+                    requestedDirection = -vessel.obt_velocity.normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.Maneuver:
+                    requestedDirection = vessel.patchedConicSolver.maneuverNodes.Count > 0 ? vessel.patchedConicSolver.maneuverNodes[0].GetBurnVector(vessel.orbit) : vessel.obt_velocity.normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.Target:
+                    requestedDirection = (vessel.targetObject.GetOrbit().getPositionAtUT(universalTime) - vesselPosition).normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.AntiTarget:
+                    requestedDirection = -(vessel.targetObject.GetOrbit().getPositionAtUT(universalTime) - vesselPosition).normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.Normal:
+                    requestedDirection = Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position)).normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.Antinormal:
+                    requestedDirection = -Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position)).normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.RadialIn:
+                    requestedDirection = -Vector3.Cross(vessel.obt_velocity, Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position))).normalized;
+                    break;
+                case VesselAutopilot.AutopilotMode.RadialOut:
+                    requestedDirection = Vector3.Cross(vessel.obt_velocity, Vector3.Cross(vessel.obt_velocity, (vesselPosition - vessel.mainBody.position))).normalized;
+                    break;
+            }
 
             if (requestedDirection == Vector3d.zero) return true;
 
-            var vesselDirection = vessel.transform.up.normalized;
-
-            if (forceRotation || Vector3d.Dot(vesselDirection, requestedDirection) > 0.9)
+            if (forceRotation || Vector3d.Dot(vessel.transform.up.normalized, requestedDirection) > 0.9)
             {
-                vessel.transform.Rotate(Quaternion.FromToRotation(vesselDirection, requestedDirection).eulerAngles, Space.World);
+                vessel.transform.Rotate(Quaternion.FromToRotation(vessel.transform.up.normalized, requestedDirection).eulerAngles, Space.World);
                 vessel.SetRotation(vessel.transform.rotation);
             }
             else
