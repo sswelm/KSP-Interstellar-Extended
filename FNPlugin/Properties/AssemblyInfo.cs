2dbf5641c835b52f4266230ac0a0c136fd4997c6
FreeThinker <sswelm@gmail.com>
2019-10-19 18:16:39 +0200

    updated to KSPIE 1.24

--- FNPlugin/Properties/AssemblyInfo.cs
+++ FNPlugin/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.23.9.4")]
-[assembly: AssemblyFileVersion("1.23.9.4")]
+[assembly: AssemblyVersion("1.24.0.5")]
+[assembly: AssemblyFileVersion("1.24.0.5")]
