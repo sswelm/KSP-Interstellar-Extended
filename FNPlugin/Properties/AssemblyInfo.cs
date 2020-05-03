03c75c5ff4285aa4225437fc2b55f69037098f0d
FreeThinker <sswelm@gmail.com>
2020-05-03 23:28:39 +0200

    updated to KSPIE 1.25.14

--- FNPlugin/Properties/AssemblyInfo.cs
+++ FNPlugin/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FNPlugin")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.25.13.5")]
-[assembly: AssemblyFileVersion("1.25.13.5")]
+[assembly: AssemblyVersion("1.25.14.5")]
+[assembly: AssemblyFileVersion("1.25.14.5")]
