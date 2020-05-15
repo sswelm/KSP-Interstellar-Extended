cb1440178c9e624df20c766ddbed7312caf6062a
sswelm <sswelm@gmail.com>
2020-05-15 10:07:55 +0200

    Added Divide by 0 checks

---
+++ FNPlugin/Extensions/BoolExtensions.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+namespace FNPlugin.Extensions
+{
+    public static class BooleanExtensions
+    {
+        public static int ToInt(this bool value)
+        {
+            return value ? 1 : 0;
+        }
+
+        public static bool IsNullOr(this bool? value, bool testValue)
+        {
+            if (value == null) return true;
+
+            return value == testValue;
+        }
+
+        [ContractAnnotation("value:false => true")]
+        public static bool IsFalse(this bool value)
+        {
+            return value == false;
+        }
+    }
+}
--- FNPlugin/Powermanagement/KspiSuperCapacitator.cs
+++ FNPlugin/Powermanagement/KspiSuperCapacitator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FNPlugin.Extensions;
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -17,16 +18,20 @@ namespace FNPlugin.Powermanagement
         public float maxStorageCapacityMJ = 0;
         [KSPField(isPersistant = false, guiActive = true, guiActiveEditor = true, guiName = "#LOC_KSPIE_KspiSuperCapacitator_Mass", guiUnits = " t")]//Mass
         public float partMass = 0;
+
         [KSPField(guiActiveEditor = false)]
         public string powerResourceName = "Megajoules";
         [KSPField(guiActiveEditor = false)]
-        public double powerConversionRate = 1000;
+        public string electricChargeResourceName = "ElectricCharge";
 
-        private double megajoulesAfterLoad = 0;
+        [KSPField]
+        public double powerConversionRate = 1000;
+        [KSPField]
+        public double megajoulesAfterLoad = 0;
 
         public override void OnStart(StartState state)
         {
-            String[] resources_to_supply = { ResourceManager.FNRESOURCE_MEGAJOULES };
+            string[] resources_to_supply = { ResourceManager.FNRESOURCE_MEGAJOULES };
             this.resources_to_supply = resources_to_supply;
 
             part.force_activate();
@@ -42,7 +47,7 @@ namespace FNPlugin.Powermanagement
             if (powerResource == null)
                 return;
 
-            var electricCharge = part.Resources["ElectricCharge"];
+            var electricCharge = part.Resources[electricChargeResourceName];
 
             if (electricCharge == null)
                 return;
@@ -50,7 +55,9 @@ namespace FNPlugin.Powermanagement
             if (megajoulesAfterLoad > 0)
             {
                 Debug.Log("[KSPI]: KspiSuperCapacitator OnSave update " + powerResourceName + " amount to " + megajoulesAfterLoad);
-                powerResource.amount = megajoulesAfterLoad;
+
+                if (megajoulesAfterLoad.IsInfinityOrNaN().IsFalse())
+                    powerResource.amount = megajoulesAfterLoad;
                 megajoulesAfterLoad = 0;
             }
 
@@ -58,28 +65,27 @@ namespace FNPlugin.Powermanagement
 
             if (powerResource.amount > 0)
             {
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnSave powerResource.amount = " + powerResource.amount);
-
                 var newElectricChargeMaxAmount = electricCharge.maxAmount + (powerResource.maxAmount * powerConversionRate);
                 var newElectricChargeAmount = electricCharge.amount + (powerResource.amount * powerConversionRate);
 
-                electricCharge.maxAmount = newElectricChargeMaxAmount;
-                electricCharge.amount = newElectricChargeAmount;
+                if (newElectricChargeMaxAmount.IsInfinityOrNaN().IsFalse())
+                {
+                    electricCharge.maxAmount = newElectricChargeMaxAmount;
+                    electricCharge.amount = newElectricChargeAmount;
+                }
 
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnSave newElectricChargeAmount = " + newElectricChargeAmount);
+                var megaJouleDecreaseMax = powerConversionRate == 0 ? 0 : availableEmptyElectricCharge / powerConversionRate;
+                var megeJouleDecrease = Math.Min(powerResource.amount, megaJouleDecreaseMax);
 
-                var megeJouleDecrease = Math.Min(powerResource.amount, availableEmptyElectricCharge / powerConversionRate);
+                var newPowerAmount = Math.Max(0, powerResource.amount - megeJouleDecrease);
 
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnSave decreased " + powerResourceName + " by " + megeJouleDecrease);
-
-                powerResource.amount = Math.Max(0, powerResource.amount - megeJouleDecrease);
+                if (newPowerAmount.IsInfinityOrNaN().IsFalse())
+                    powerResource.amount = newPowerAmount;
             }
         }
 
         public override void OnLoad(ConfigNode node)
         {
-            Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad start");
-
             if (HighLogic.LoadedSceneIsEditor)
                 return;
 
@@ -88,42 +94,31 @@ namespace FNPlugin.Powermanagement
             if (powerResource == null)
                 return;
 
-            var electricCharge = part.Resources["ElectricCharge"];
+            var electricCharge = part.Resources[electricChargeResourceName];
 
             if (electricCharge == null)
                 return;
 
             if (part.protoPartSnapshot == null)
-            {
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad part.protoPartSnapshot == null");
                 return;
-            }
 
             // detect amount of power used offline and adjust megajoules
-            var protoElectricCharge = part.protoPartSnapshot.resources.FirstOrDefault(m => m.resourceName == "ElectricCharge");
+            var protoElectricCharge = part.protoPartSnapshot.resources.FirstOrDefault(m => m.resourceName == electricChargeResourceName);
 
             if (protoElectricCharge == null)
-            {
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad protoElectricCharge == null");
                 return;
-            }
-
-            Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad protoElectricCharge " + protoElectricCharge.amount);
 
             var protoPowerResource = part.protoPartSnapshot.resources.FirstOrDefault(m => m.resourceName == powerResourceName);
 
             if (protoPowerResource != null)
             {
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad protoPowerResource maxAmount " + protoPowerResource.maxAmount);
-                Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad protoPowerResource amount " + protoPowerResource.amount);
-
                 powerResource.maxAmount = protoPowerResource.maxAmount;
                 powerResource.amount = protoPowerResource.amount;
             }
 
-            megajoulesAfterLoad = Math.Max(0, (protoElectricCharge.amount - electricCharge.maxAmount) / (protoElectricCharge.maxAmount - electricCharge.maxAmount)) * powerResource.maxAmount;
+            var deltaElectricChargeMaxAmount = protoElectricCharge.maxAmount - electricCharge.maxAmount;
 
-            Debug.Log("[KSPI]: KspiSuperCapacitator OnLoad megajoulesAfterLoad " + megajoulesAfterLoad);
+            megajoulesAfterLoad = deltaElectricChargeMaxAmount == 0 ? 0 : Math.Max(0, (protoElectricCharge.amount - electricCharge.maxAmount) / deltaElectricChargeMaxAmount) * powerResource.maxAmount;
         }
     }
 }
