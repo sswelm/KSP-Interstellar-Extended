2924e73bb3b65fc6cc3eaed4d4e0eb1f4d4e1a03
sswelm <sswelm@gmail.com>
2020-08-22 21:47:30 +0200

    increased version IFS to 3.17

--- FuelSwitch/Properties/AssemblyInfo.cs
+++ FuelSwitch/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Interstellar FuelSwitch")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.15.0.5")]
-[assembly: AssemblyFileVersion("3.15.0.5")]
+[assembly: AssemblyVersion("3.17.0.5")]
+[assembly: AssemblyFileVersion("3.17.0.5")]
