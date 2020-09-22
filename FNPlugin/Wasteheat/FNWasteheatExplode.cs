900c90c177a4847bbce2a39150797c845d2bbc18
sswelm <sswelm@gmail.com>
2020-09-22 20:30:54 +0200

    Improved heat explosion

--- FNPlugin/Wasteheat/FNWasteheatExplode.cs
+++ FNPlugin/Wasteheat/FNWasteheatExplode.cs
@@ -1,36 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace FNPlugin.Wasteheat 
+﻿namespace FNPlugin.Wasteheat 
 {
-    class FNWasteheatExplode : PartModule 
+    class FNWasteheatExplode : PartModule
     {
+        [KSPField] 
+        public bool activeOnlyWhenActivated = true;
         [KSPField]
-        public double explodeFrame = 30;
+        public double explodeFrame = 25;    // half a second
         [KSPField]
         public double explodeRatio = 1;
-        
+        [KSPField(guiActiveEditor = true, guiActive = true, guiName = "Explosion Potential")]
+        public float explosionPotential;
 
-        private int explode_counter;
+        private int _explodeCounter;
 
-        public override void OnStart(PartModule.StartState state)
+        public override void OnStart(StartState state)
         {
+            if (explosionPotential <= 0)
+                explosionPotential = part.explosionPotential;
+        }
+
+        public void FixedUpdate()
+        {
+            if (HighLogic.LoadedSceneIsEditor) return;
+
+            if (!enabled && activeOnlyWhenActivated == false)
+                base.OnFixedUpdate();
         }
 
         public override void OnFixedUpdate() // OnFixedUpdate is only called when (force) activated
         {
-            var wasteheatResource = part.Resources["WasteHeat"];
+            var wasteheatResource = part.Resources[ResourceManager.FNRESOURCE_WASTEHEAT];
 
             if (!CheatOptions.IgnoreMaxTemperature && wasteheatResource != null && wasteheatResource.amount >= wasteheatResource.maxAmount * explodeRatio)
             {
-                explode_counter++;
-                if (explode_counter > explodeFrame)
-                    part.explode();
+                _explodeCounter++;
+                if (_explodeCounter < explodeFrame) return;
+
+                part.explosionPotential = explosionPotential;
+                part.explode();
             }
             else
-                explode_counter = 0;
+                _explodeCounter = 0;
         }
     }
 }
