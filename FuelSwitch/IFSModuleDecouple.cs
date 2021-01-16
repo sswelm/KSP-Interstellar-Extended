0ce686c091086244d27ddf7d35138dcee95e2822
sswelm <sswelm@gmail.com>
2021-01-16 15:43:45 +0100

    Fixed several warninh and error message

---
+++ FuelSwitch/IFSModuleDecouple.cs
@@ -0,0 +1,16 @@
+﻿namespace InterstellarFuelSwitch
+{
+    class IFSModuleDecouple : ModuleDecouple
+    {
+        public override void OnAwake()
+        {
+            fx = part.findFxGroup(fxGroupName);
+
+            if (menuName == string.Empty)
+                return;
+
+            Events[nameof(Decouple)].guiName = menuName;
+            Actions[nameof(DecoupleAction)].guiName = menuName;
+        }
+    }
+}
